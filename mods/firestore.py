cd695ab3bee34e4054cfbe2e2409f7fcb322a4ed
spesthecat <kenhtsun@gmail.com>
2020-03-13 19:08:54 -0400

    Moving token to its own module; starting separate module for db

--- mods/firestore.py
+++ mods/firestore.py
@@ -1,14 +1,16 @@
 import firebase_admin
-from firebase_admin import auth
+from firebase_admin import firestore
+
 
 app = firebase_admin.initialize_app()
+db = firestore.client(app)
 
 
-def create_custom_token(uid):
-    custom_token = auth.create_custom_token(uid)
-    return custom_token
+def _write(path, data, merge=True):
+    doc_ref = db.document(path)
+    doc_ref.set(data, merge=merge)
 
 
-def refresh_custom_token(uid):
-    auth.revoke_refresh_tokens(uid)
-    return create_custom_token(uid)
+def _update(path, data):
+    doc_ref = db.document(path)
+    doc_ref.update(data)
