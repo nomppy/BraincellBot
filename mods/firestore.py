176917cc4b911e094378895d59e93416fbc05321
spesthecat <kenhtsun@gmail.com>
2020-03-13 18:07:02 -0400

    beginning of firestore module

---
+++ mods/firestore.py
@@ -0,0 +1,14 @@
+import firebase_admin
+from firebase_admin import auth
+
+app = firebase_admin.initialize_app()
+
+
+def create_custom_token(uid):
+    custom_token = auth.create_custom_token(uid)
+    return custom_token
+
+
+def refresh_custom_token(uid):
+    auth.revoke_refresh_tokens(uid)
+    return create_custom_token(uid)
