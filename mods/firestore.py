a927c6f03a5b8fbd68773977ebc580fcfbf76948
spesthecat <kenhtsun@gmail.com>
2020-03-14 07:47:59 -0400

    adding functions to interact with firestore

--- mods/firestore.py
+++ mods/firestore.py
@@ -7,10 +7,26 @@ db = firestore.client(app)
 
 
 def _write(path, data, merge=True):
-    doc_ref = db.document(path)
-    doc_ref.set(data, merge=merge)
+    ref = db.document(path)
+    ref.set(data, merge=merge)
 
 
 def _update(path, data):
-    doc_ref = db.document(path)
-    doc_ref.update(data)
+    ref = db.document(path)
+    ref.update(data)
+
+
+def _delete(path):
+    db.document(path).delete()
+
+
+def delete_collection(coll_ref, batch_size):
+    docs = coll_ref.limit(batch_size).stream()
+    deleted = 0
+
+    for doc in docs:
+        doc.reference.delete()
+        deleted += 1
+
+    if deleted >= batch_size:
+        return delete_collection(coll_ref, batch_size)
