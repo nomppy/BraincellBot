6860dd881233a1c7d407a7526a5104e5bee6f54c
spesthecat <kenhtsun@gmail.com>
2020-03-25 14:18:24 -0400

    finna kms

--- exts/restart.py
+++ exts/restart.py
@@ -6,8 +6,8 @@ from discord.ext import commands
 @commands.command()
 @commands.is_owner()
 async def restart(ctx):
-    ctx.send('Restarting...')
-    os.system('./restart_bot.sh')
+    await ctx.send('Restarting...')
+    os.system('./restart_bot.bash')
 
 
 def setup(bot):
