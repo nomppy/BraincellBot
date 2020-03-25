5690331bcc10213fcc285f56f784d7f09692f5bf
spesthecat <kenhtsun@gmail.com>
2020-03-25 18:43:44 +0000

    fixing restart... again... for the last time hopefully

--- restart_bot.bash
+++ restart_bot.bash
@@ -2,4 +2,4 @@
 tmux kill-session -t bot
 tmux new -s bot -d 
 tmux send-keys -t bot "source ./venv/bin/activate" C-m
-tmux send-keys -t bot "python BraincellBot.py" C-m
+tmux send-keys -t bot "python BraincellBot.py &" C-m
