f4e79e405ed8c138a2c66723efc5724231d3d39d
spesthecat <kenhtsun@gmail.com>
2020-03-26 19:27:43 -0400

    afas

--- mods/vars_.py
+++ mods/vars_.py
@@ -10,4 +10,3 @@ colour_error = discord.Colour.from_rgb(161, 35, 44)
 colour_warning = discord.Colour.from_rgb(209, 200, 31)
 
 default_footer_text = 'created and maintained by Spes#0845'
-default_footer_icon = ''
