f9588a4e3c9627821abf0e3294f78695a00145ae
spesthecat <kenhtsun@gmail.com>
2020-04-02 17:46:26 -0400

    adding cooldown to meow

--- exts/meow.py
+++ exts/meow.py
@@ -8,6 +8,7 @@ aliases = ['catpls', 'plscat', 'bestanimal', 'cat']
 
 
 @commands.command(aliases=aliases)
+@commands.cooldown(1, 4, commands.BucketType.member)
 async def meow(ctx):
     if ctx.author.bot:
         return
@@ -16,6 +17,19 @@ async def meow(ctx):
         link = await get_cat_link()
         await ctx.send(embed=discord.Embed().set_image(url=link))
     await firestore.update_command_field(str(ctx.author.id), 'meow', '_last', link)
+
+
+@meow.error
+async def meow_err(ctx, err):
+    emb = discord.Embed(
+        title='Meow',
+        colour=vars_.colour_error
+    )
+    if isinstance(err, commands.CommandOnCooldown):
+        emb.description = f'On cooldown for another {err.retry_after} seconds'
+    else:
+        emb.description = 'An unknown error has occurred'
+    await ctx.send(embed=emb)
 
 
 def setup(bot):
