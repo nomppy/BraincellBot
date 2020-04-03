e92de7ef2e9e0fd9de5ec9c5046702c936a15f24
spesthecat <kenhtsun@gmail.com>
2020-04-03 07:40:12 -0400

    adding invite command

---
+++ exts/invite.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from mods import info, vars_
+
+
+@commands.command()
+async def invite(ctx):
+    await ctx.send(embed=discord.Embed(
+        title='Click here!',
+        colour=ctx.bot.guild.me.colour,
+        url='https://discordapp.com/api/oauth2/authorize?client_id=681128428195282947&permissions=117824&scope=bot'
+    ))
+
+
+def setup(bot):
+    info.Info(
+        name='invite',
+        brief='invite me to your server',
+        usage='`invite`',
+    ).export(vars_.info_)
+    bot.add_command(invite)
