fbae965b41694ace043b01bec1b48fdd821fdaf4
spesthecat <kenhtsun@gmail.com>
2020-04-03 07:33:45 -0400

    adding reset command

--- exts/update.py
+++ exts/update.py
@@ -6,7 +6,10 @@ from exts import restart
 
 @commands.command()
 @commands.is_owner()
-async def update(ctx):
+async def update(ctx, arg=None):
+    if arg in ['-r', 'reset', '--reset']:
+        await ctx.send('resetting to origin/master')
+        os.system('git reset --hard origin/master')
     await ctx.send('Pulling changes from origin/master...')
     os.system('git pull')
     await restart.restart(ctx)
