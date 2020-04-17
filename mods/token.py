9be81e837d296b89b06ed6c57f806dc5caf931e2
spesthecat <kenhtsun@gmail.com>
2020-04-17 12:59:08 -0400

    fixing fix

--- mods/token.py
+++ mods/token.py
@@ -13,7 +13,7 @@ except ValueError:
 
 
 def create_custom_token(uid: str):
-    auth.create_user(uid=uid)
+    auth.create_user(uid=uid, app=app)
     custom_token = auth.create_custom_token(uid, app=app)
     return custom_token
 
